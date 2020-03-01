60368ff58fd337abb74d2861d7d4f053e7a7d53a
leunk <leunk@knights.ucf.edu>
2020-02-29 20:34:09 -0500

    started lab4

---
+++ labs/4/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Card from "./components/Card";
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <Card content="This is a card!"></Card>
+      </div>
+    );
+  }
+}
+
+export default App;
