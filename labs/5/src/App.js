0a5c3d7e3b3d595ef33f2f6a0d6faee9b7ed08e6
leunk <leunk@knights.ucf.edu>
2020-03-28 00:28:48 -0400

    fixed it

--- labs/5/src/App.js
+++ labs/5/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 //react-native can't compile
 // import { View } from 'react-native';
 // import Weather from './components/Weather';
-import Weather from './components/Weather';
+import Card from './components/Card';
 
 class App extends React.Component{
   render(){
     return(
    
-        <Weather />
+        <Card />
     );
   }
 }
